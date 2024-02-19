7c46b58fae671bf630786a8309e113e7d33f23a6
CathyyRyuu <cathyliu0315@hotmail.com>
2024-02-18 23:39:26 -0800

    temporarily remove email function since domain failed

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,15 +1,15 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import GithubIcon from "../../public/github-icon.svg";
 import LinkedinIcon from "../../public/linkedin-icon.svg";
-import Link from 'next/link';
+import Link from "next/link";
 import Image from "next/image";
 
-// need domain set up.
+// need domain to set up.
 
 const EmailSection = () => {
   // change this emailSubmitted to false when email function is implemented
-  const [emailSubmitted, setEmailSubmitted] = useState(true);
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,39 +17,42 @@ const EmailSection = () => {
       email: e.target.email.value,
       subject: e.target.subject.value,
       message: e.target.message.value,
-    }
+    };
     const JSONdata = JSON.stringify(data);
     const endpoint = "/api/send";
 
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSONdata,
-    }
+    };
 
     const response = await fetch(endpoint, options);
     const resData = await response.json();
-    console.log('resData');
+    console.log("resData");
 
     if (response.status === 200) {
-      console.log('Message sent.');
+      console.log("Message sent.");
       setEmailSubmitted(true);
     }
   };
 
   return (
-    <section id="contact" className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
-      <div className='bg-radial-gradient(ellipse at center,_var(--tw-gradient-stops)) from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2'></div>
-      <div className='z-10'>
-        <h5 className='text-xl font-bold text-white my-2'>Let's Connect !</h5>
-        <p className='text-[#ADB7BE] mb-4 max-w-md'>
+    <section
+      id="contact"
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+    >
+      <div className="bg-radial-gradient(ellipse at center,_var(--tw-gradient-stops)) from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+      <div className="z-10">
+        <h5 className="text-xl font-bold text-white my-2">Let's Connect !</h5>
+        <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I'm always looking for new opportunities in software development.
-          Feel free to connect with me!
+          I'm always looking for new opportunities in software development. Feel
+          free to connect with me!
         </p>
-        <div className='socials flex flex-row gap-2'>
+        <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/CathyyRyuu">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
@@ -59,65 +62,71 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className='flex flex-col' onSubmit={handleSubmit}>
-          <div className='mb-6'>
-            <label 
-              htmlFor='email' 
-              className='text-white block mb-2 font-medium'>
+        {/* <form className='flex flex-col' onSubmit={handleSubmit}> */}
+        <form className="flex flex-col">
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="text-white block mb-2 font-medium"
+            >
               Your Email
             </label>
             <input
-              name='email'
-              type='email'
-              id='email'
+              name="email"
+              type="email"
+              id="email"
               required
-              className='bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-              placeholder='user@example.com'
+              className="bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="user@example.com"
             />
           </div>
 
-          <div className='mb-6'>
-            <label 
-              htmlFor='subject' 
-              className='text-white block mb-2 font-medium'>
+          <div className="mb-6">
+            <label
+              htmlFor="subject"
+              className="text-white block mb-2 font-medium"
+            >
               Subject
             </label>
             <input
-              name='subject'
-              type='text'
-              id='subject'
+              name="subject"
+              type="text"
+              id="subject"
               required
-              className='bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-              placeholder='Hi there!'
+              className="bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Hi there!"
             />
           </div>
 
-          <div className='mb-6'>
-            <label 
-              htmlFor='message' 
-              className='text-white block mb-2 font-medium'>
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="text-white block mb-2 font-medium"
+            >
               Message
             </label>
             <textarea
-              name='message'
-              id='message'
+              name="message"
+              id="message"
               required
-              className='bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+              className="bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
               placeholder="Let's grab a coffee!"
             />
           </div>
+          <p className="text-red-400 text-sm mt-2 mb-6">
+            Sorry, Email functionality is currently unavailable.
+          </p>
           <button
             type="submit"
-            className='opacity-75 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:opacity-100 text-white font-medium py-2.5 px-5 rounded-lg w-full'>
-              Send Message
+            className="opacity-75 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:opacity-100 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+          >
+            Send Message
           </button>
-          {
-            emailSubmitted && (
-              <p className='text-green-300 text-sm mt-2'>
-                Email sent successfully!
-              </p>
-            )
-          }
+          {emailSubmitted && (
+            <p className="text-green-300 text-sm mt-2">
+              Email sent successfully!
+            </p>
+          )}
         </form>
       </div>
     </section>
